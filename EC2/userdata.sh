12cd9a9efe3d306cb8064e179c13cf50c2176793
SROR7 <m7mdsror@gmail.com>
2026-07-28 04:57:05 +0300

    test

--- EC2/userdata.sh
+++ EC2/userdata.sh
@@ -10,7 +10,7 @@ elif command -v apt-get &> /dev/null; then
     apt-get update -y
     apt-get install -y apache2
     systemctl enable apache2
-    systemctl start apache
+    systemctl start apache2
 fi
 
 # Create HTML page
