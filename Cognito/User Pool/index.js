c339486dfa4057d562beb763c551aad6b93d79be
SROR7 <m7mdsror0@gmail.com>
2026-09-07 19:48:03 +0300

    create a userPool

---
+++ Cognito/User Pool/index.js
@@ -0,0 +1,24 @@
+// index.js
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { AuthProvider } from "react-oidc-context";
+
+const cognitoAuthConfig = {
+  authority: "https://cognito-idp.eu-north-1.amazonaws.com/eu-north-1_2k5VnQYic",
+  client_id: "6ih0qesrd5og575k1c4uq4orus",
+  redirect_uri: "https://myapp.co",
+  response_type: "code",
+  scope: "phone openid email",
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// wrap the application with AuthProvider
+root.render(
+  <React.StrictMode>
+    <AuthProvider {...cognitoAuthConfig}>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
+);
---
+++ Cognito/User Pool/readme.md
@@ -0,0 +1,5 @@
+## Configure your user pool app client
+
+```sh 
+npm install oidc-client-ts react-oidc-context --save
+```
